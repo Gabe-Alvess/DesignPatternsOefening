fd0e5eb71eb286264807938a403160d54be82a88
Gabriel Alves <gabriel_vieira2018@hotmail.com>
2023-06-02 12:22:38 +0200

    update

--- src/main/java/be/intecbrussel/strategypattern/MergeSortAlgorithm.java
+++ src/main/java/be/intecbrussel/strategypattern/MergeSortAlgorithm.java
@@ -3,6 +3,43 @@ package be.intecbrussel.strategypattern;
 public class MergeSortAlgorithm implements ArraySortingAlgorithm {
     @Override
     public int[] sortArray(int[] array) {
-        return new int[0];
+        int length = array.length;
+
+        if (length < 2) {
+            throw new IllegalArgumentException("Error - Invalid Array");
+        }
+
+        int mid = length / 2;
+        int[] lHalf = new int[mid];
+        int[] rHalf = new int[length - mid];
+
+        for (int i = 0; i < mid; i++) {
+            lHalf[i] = array[i];
+        }
+        for (int i = mid; i < length; i++) {
+            rHalf[i - mid] = array[i];
+        }
+
+        merge(array, lHalf, rHalf, mid, length - mid);
+
+        return array;
+    }
+
+    private void merge(int[] array, int[] lHalf, int[] rHalf, int leftL, int rightL) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < leftL && j < rightL) {
+            if (lHalf[i] <= rHalf[j]) {
+                array[k++] = lHalf[i++];
+            } else {
+                array[k++] = rHalf[j++];
+            }
+        }
+        while (i < leftL) {
+            array[k++] = lHalf[i++];
+        }
+        while (j < rightL) {
+            array[k++] = rHalf[j++];
+        }
     }
 }
--- src/main/java/be/intecbrussel/strategypattern/SorterApp.java
+++ src/main/java/be/intecbrussel/strategypattern/SorterApp.java
@@ -5,25 +5,28 @@ import java.util.Arrays;
 public class SorterApp {
     public static void main(String[] args) {
 
-        int[] arr = {3, 60, 35, 2, 45, 320, 5};
+        int[] arr1 = {3, 60, 35, 2, 45, 320, 5};
 
-        System.out.println("Unsorted array -> " + Arrays.toString(arr));
-
-        line();
-
-        ArraySorter arraySorter = new ArraySorter(new BubbleSortAlgorithm());
-
-        System.out.println("Sorted array -> " + Arrays.toString(arraySorter.sortMyArray(arr)));
+        System.out.println("Unsorted array -> " + Arrays.toString(arr1));
 
         line();
 
-        System.out.println("Unsorted array -> " + Arrays.toString());
+        ArraySorter arraySorter1 = new ArraySorter(new BubbleSortAlgorithm());
+
+        System.out.println("Sorted with bubble algorithm -> " + Arrays.toString(arraySorter1.sortMyArray(arr1)));
+
+        line();
+        int[] arr2 = {10, 6, 8, 5, 7, 3, 4};
+
+        System.out.println("Unsorted array -> " + Arrays.toString(arr2));
 
         line();
 
+        ArraySorter arraySorter2 = new ArraySorter(new MergeSortAlgorithm());
 
-        System.out.println("Sorted array -> " + Arrays.toString());
+        System.out.println("Sorted with merge algorithm -> " + Arrays.toString(arraySorter2.sortMyArray(arr2)));
     }
+
     public static void line() {
         System.out.println("-".repeat(50));
     }
